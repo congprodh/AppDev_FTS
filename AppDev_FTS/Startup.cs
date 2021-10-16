904cd1027d0c3fd2830c970dd381fc5e201c6cd2
congprodh <congprodh@gmail.com>
2021-10-16 10:32:31 +0700

    Add project files.

---
+++ AppDev_FTS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppDev_FTS.Startup))]
+namespace AppDev_FTS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
