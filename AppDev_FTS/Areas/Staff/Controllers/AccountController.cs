d5414ba6abaaaac9f7b944240f13ccffa39ccd3c
nnhi9428 <nnhi9428@gmail.com>
2021-10-18 19:59:46 +0700

    Add Index and AccountController by Staff

---
+++ AppDev_FTS/Areas/Staff/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AppDev_FTS.Models;
+using AppDev_FTS.Utils;
+using AppDev_FTS.ViewModels;
+
+namespace AppDev_FTS.Areas.Staff.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private readonly ApplicationDbContext _context;
+
+        // GET: Staff/Account
+        public AccountController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public async Task<ActionResult> Index()
+        {
+         
+            var traineeRole = await _context.Roles.SingleOrDefaultAsync(r => r.Name == Role.Trainee);
+
+            var model = new UsersGroupViewModel
+            {
+                Trainees = await _context.Users
+                    .Where(u => u.Roles.Any(r => r.RoleId == traineeRole.Id))
+                    .ToListAsync(),
+            };
+
+            return View(model);
+        }
+
+       
+}
--- AppDev_FTS/ViewModels/UsersGroupViewModel.cs
+++ AppDev_FTS/ViewModels/UsersGroupViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 
 namespace AppDev_FTS.ViewModels
 {
+    //Admin
     public class UsersGroupViewModel
     {
         public List<ApplicationUser> Staffs { get; set; }
 
         public List<ApplicationUser> Trainers { get; set; }
+        public List<ApplicationUser> Trainees { get; set; }
     }
 }
